97ba8cf5f2e10024dd39a86db41b30e47406cc59
Vishnu Murthy <vmurthy@berkeley.edu>
2015-12-09 00:41:50 -0800

    Dad?

--- static/dashboard.js
+++ static/dashboard.js
@@ -140,7 +140,7 @@ function loadContent(){
 		'    </div>'+
 		'	   <div class="col-md-8 content-left">'+
 		'      On this page you can find information regarding Emission Savings and Fridge Openings </div>'+
-		'	   <div class="col-md-4 content-right"><img src="../histogram.jpg" alt="Fridge Analytics"></div>'+
+		'	   <div class="col-md-4 content-right"><img src="histogram.jpg" alt="Fridge Analytics"></div>'+
 		'  </div>'+
 		'</div>');
 	$('div#content').append( contentAnalytics );
